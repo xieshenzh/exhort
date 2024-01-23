c5c4e785fc5c57720bc3ea3d55c57121073c0096
Olga Lavtar <olavtar@redhat.com>
2024-01-23 12:36:25 -0800

    New Docker HTML Report.
    Signed-off-by: Olga Lavtar <olavtar@redhat.com>

--- ui/src/mocks/reportWithForbidden.mock.ts
+++ ui/src/mocks/reportWithForbidden.mock.ts
@@ -2,31 +2,32 @@ import { AppData } from '@app/api/report';
 
 export const forbiddenReport: AppData = {
   providerPrivateData: null,
-  report: {
-    "scanned": {
-      "total": 9,
-      "direct": 2,
-      "transitive": 7
-    },
-    "providers": {
-      "oss-index": {
-        "status": {
-          "ok": false,
-          "name": "oss-index",
-          "code": 403,
-          "message": "Forbidden: The provided credentials don't have the required permissions."
-        }
-      },
-      "snyk": {
-        "status": {
-          "ok": false,
-          "name": "snyk",
-          "code": 403,
-          "message": "Forbidden: The provided credentials don't have the required permissions."
-        }
-      }
-    }
-  },
+  report: [],
+  // report: {
+  //   "scanned": {
+  //     "total": 9,
+  //     "direct": 2,
+  //     "transitive": 7
+  //   },
+  //   "providers": {
+  //     "oss-index": {
+  //       "status": {
+  //         "ok": false,
+  //         "name": "oss-index",
+  //         "code": 403,
+  //         "message": "Forbidden: The provided credentials don't have the required permissions."
+  //       }
+  //     },
+  //     "snyk": {
+  //       "status": {
+  //         "ok": false,
+  //         "name": "snyk",
+  //         "code": 403,
+  //         "message": "Forbidden: The provided credentials don't have the required permissions."
+  //       }
+  //     }
+  //   }
+  // },
   ossIssueTemplate: 'http://ossindex.sonatype.org/vulnerability/__ISSUE_ID__',
   snykIssueTemplate: 'https://security.snyk.io/vuln/__ISSUE_ID__',
   snykSignup: 'https://app.snyk.io/login',
